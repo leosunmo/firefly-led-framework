6a048a2d1b9ba8eaa6d26151315a3a7f82852e14
Carlos Jones <cjonesii@yahoo.com>
2024-08-24 14:33:02 +0800

    Replace manual fifo buffer fill

--- source/kylarLEDs/Controllers/Sensors/Microphone/pdm_mic/audio_spectrogram.c
+++ source/kylarLEDs/Controllers/Sensors/Microphone/pdm_mic/audio_spectrogram.c
@@ -79,8 +79,9 @@ void pdm_init(){
 void __not_in_flash_func(adc_capture)(uint16_t *buf, size_t count) {
     adc_fifo_setup(true, false, 0, false, false);
     adc_run(true);
-    for (int i = 0; i < count; i = i + 1)
-        buf[i] = adc_fifo_get_blocking();
+    // for (int i = 0; i < count; i = i + 1)
+    //     buf[i] = adc_fifo_get_blocking();
+    dma_channel_wait_for_finish_blocking(dma_chan);
     adc_run(false);
     adc_fifo_drain();
 }
