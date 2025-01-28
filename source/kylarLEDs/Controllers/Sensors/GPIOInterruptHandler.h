698e3c74efff5aad6283ba8d6413635b3a1a530c
Leo Palmer <leps@boozt.com>
2025-01-28 21:20:38 +0100

    Add new GPIO interrupt handler
    This allows us to trigger interrupts per GPIO rather than a global
    interrupt for _any_ GPIO

---
+++ source/kylarLEDs/Controllers/Sensors/GPIOInterruptHandler.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdint.h"
+#include "pico/stdlib.h"
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+class GPIOInterruptHandler {
+public:
+    static void registerCallback(uint gpio, std::function<void()> callback);
+    static void handleInterrupt(uint gpio, uint32_t event);
+
+private:
+    static std::unordered_map<uint, std::vector<std::function<void()>>> pinCallbacks;
+};
+
+extern "C" void gpio_callback(uint gpio, uint32_t event);
