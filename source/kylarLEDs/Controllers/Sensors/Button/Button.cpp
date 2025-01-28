360069d0821fbf146bd9fe6ce4636ce1070a6540
Leo Palmer <leps@boozt.com>
2025-01-28 21:21:16 +0100

    Refactor button and encoder
    Use new GPIO interrupt handler and use callbacks to allow us to pass
    these inputs to other classes

--- source/kylarLEDs/Controllers/Sensors/Button/Button.cpp
+++ source/kylarLEDs/Controllers/Sensors/Button/Button.cpp
@@ -3,51 +3,49 @@
 #include "hardware/gpio.h"
 #include "pico/time.h"
 #include "stdio.h"
-#include "../Encoder/Encoder.h"
 
-uint8_t Button::pin;
-uint32_t* Button::patternIndex;
+std::vector<std::function<void()>> Button::callbacks;
 
-void Button::interrupt(uint gpio, uint32_t event){
-    if(gpio != pin){
-        Encoder::interrupt(gpio, event);
-        return;
-    }
+Button::Button(int pin) : pin(pin)
+{
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_pull_up(pin);
+    GPIOInterruptHandler::registerCallback(pin, [this]()
+                                           { this->handleInterrupt(); });
+}
+
+void Button::setCallback(std::function<void()> callback)
+{
+    Button::callbacks.push_back(callback);
+}
+
+void Button::handleInterrupt()
+{
     static absolute_time_t last_time = {0};
-    static int press_state = 0;// 0 Waiting for press, 1 waiting for release
+    static int press_state = 0; // 0 Waiting for press, 1 waiting for release
     absolute_time_t new_time = get_absolute_time();
-    
-
-    if(absolute_time_diff_us(last_time, new_time) < 30000){ //30ms
+    // Debounce
+    if (absolute_time_diff_us(last_time, new_time) < 30000)
+    {
+        // 30ms debounce
         return;
-    }
-
-
-    if(press_state == 0){
-        (*Button::patternIndex)++; // Increment pattern index
-        press_state = 1;
-    }else{
-        press_state = 0;
     }
 
     last_time = new_time;
-    
-}
 
-void Button::givePatternIndex(uint32_t *patternIndex){
-    Button::patternIndex = patternIndex;
-}
+    if (press_state == 0)
+    {
+        press_state = 1;
+    }
+    else
+    {
+        press_state = 0;
+    }
 
-
-Button::Button(int A){
-    //Inputs:
-    Button::pin = A;
-    gpio_init(A);
-    gpio_set_dir(A, GPIO_IN);
-    gpio_pull_up(A);//Set PULL UP
-    //INTT//INTERRUPTS NOTE: "Currently GPIO parameter is ignored, callback will be called for any enabled GPIO IRQ on any pin"
-    //This is spaghetti code, to fix this I am thinking to make a static "Interrupts" class that all interrupts can be handled by 1
-    //The objects would be added to the Interrupts class static list so that all of their interrupt functions get called or basedon their gpio
-    gpio_irq_callback_t callback = &Button::interrupt;
-    gpio_set_irq_enabled_with_callback(A, GPIO_IRQ_EDGE_FALL, true, callback);
+    // Call all callbacks
+    for (const auto &callback : Button::callbacks)
+    {
+        callback();
+    }
 }
